4e97a32b30990381296390ab740d05db37dbe8cc
EmilioVP <emiliovpsis@gmail.com>
2024-08-27 17:14:35 -0600

    interfaz de usuario main

--- src/main/java/com/example/tap2024/HelloApplication.java
+++ src/main/java/com/example/tap2024/HelloApplication.java
@@ -8,10 +8,9 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
-    @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(new Button(text = "hola :)"), 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
