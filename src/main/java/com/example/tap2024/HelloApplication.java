69e6ab10021c558d90d7c03c57cd132538ff0a4d
EmilioVP <emiliovpsis@gmail.com>
2024-08-30 09:47:52 -0600

    menus

--- src/main/java/com/example/tap2024/HelloApplication.java
+++ src/main/java/com/example/tap2024/HelloApplication.java
@@ -1,41 +1,50 @@
 package com.example.tap2024;
 
+import com.example.tap2024.vistas.Calculadora;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import java.awt.*;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
 
-    private Button btn1, btn2, btn3;
-    private VBox vbox;
-    private HBox hbox;
+    private BorderPane bdpPrincipal;
+    private MenuBar mnbPrincipal;
+    private Menu menComp1, menComp2, menSalir;
+    private MenuItem mitCalculadora;
 
     public void CrearUI8(){
-        btn1 = new Button("boton 1");
-        btn2 = new Button("Boton 2");
-        btn3 = new Button("Boton 3");
-        vbox = new VBox(btn1, btn2, btn3);
-        hbox = new HBox(btn1, btn2, btn3);
+
+        mitCalculadora = new MenuItem("Calculadora");
+        mitCalculadora.setOnAction(actionEvent -> new Calculadora());
+        menComp1 = new Menu("Competencia 1");
+        menComp1.getItems().add(mitCalculadora);
+        mnbPrincipal = new MenuBar(menComp1);
+        bdpPrincipal = new BorderPane();
+        bdpPrincipal.setTop(mnbPrincipal);
+
     }
 
     @Override
     public void start(Stage stage) throws IOException {
         //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         CrearUI8();
-        Scene scene = new Scene(hbox, 320, 240);
+        Scene scene = new Scene(bdpPrincipal, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.setMaximized(true);
         stage.show();
     }
 
-    public static void main(String[] args) {
+    public static void main(String... args) {
         launch();
     }
 }
--- src/main/java/com/example/tap2024/vistas/Calculadora.java
+++ src/main/java/com/example/tap2024/vistas/Calculadora.java
@@ -1,5 +1,6 @@
 package com.example.tap2024.vistas;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
@@ -9,24 +10,49 @@ import javafx.stage.Stage;
 
 public class Calculadora extends Stage {
 
-   private Button[] arrbtn;
+   private Button[][] arrbtn;
    private TextField txtPantalla;
    private GridPane teclado;
    private VBox vbox;
    private Scene escena;
+   private String[] strTeclas = {"7", "8", "9", "*", "4", "5", "6", "/", "1", "2", "3", "+", "0", ".", "=", "-"};
 
    private void CrearUI(){
-       arrbtn = new Button[16];
-       txtPantalla = new TextField("0");
+       arrbtn = new Button[4][4];
+       txtPantalla = new TextField("");
+       txtPantalla.setAlignment(Pos.CENTER_RIGHT);
+       txtPantalla.setEditable(false);
        teclado = new GridPane();
-       vbox = new VBox();
-       escena = new Scene();
+       CrearTeclado();
+       vbox = new VBox(txtPantalla, teclado);
+       escena = new Scene(vbox,300,350);
 
    }
 
-   public calc(){
+   private void CrearTeclado(){
+       for (int i = 0; i < arrbtn.length; i++) {
+           for (int j = 0; j < arrbtn.length; j++) {
+               arrbtn[j][i] = new Button(strTeclas[4*i + j]);
+               arrbtn[j][i].setPrefSize(100,100);
+               int finalI = i;
+               int finalJ = j;
+               arrbtn[j][i].setOnAction(event -> DetectarTecla(strTeclas[4* finalI + finalJ]));
+               teclado.add(arrbtn[j][i],j,i);
+
+           }
+
+       }
+   }
+
+   public Calculadora(){
+        CrearUI();
         this.setTitle("Calculadora");
-        this.setScene();
+        this.setScene(escena);
         this.show();
+   }
+
+   private void DetectarTecla(String tecla){
+       //txtPantalla.setText("");
+       txtPantalla.appendText(tecla);
    }
 }
